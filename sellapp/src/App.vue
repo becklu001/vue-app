90221f03fb67d29dbe5f64ec121f305a3af2e22b
becklu001 <becklu001@hotmail.com>
2019-10-21 11:26:14 +0800

    1020edit

---
+++ sellapp/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+   <!--<img src="./assets/logo.png">-->
+    <!--<router-view/>-->
+    <header>
+      i am header
+    </header>
+    <div class="tab">
+      tab
+    </div>
+    <div class="content">
+      i am content
+    </div>
+  </div>
+</template>
+
+<script>
+import header from './components/header/header.vue'
+export default {
+  header
+//  'v-header': header
+//  name: 'v-header'
+}
+</script>
+
+<style>
+/*#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}*/
+</style>
---
+++ sellapp/src/components/header/header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="header">
+    我是Header
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default{
+  name: 'header'
+}
+</script>
+
+<style>
+</style>
